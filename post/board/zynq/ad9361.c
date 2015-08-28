77f2af16fffe411cc657cc472dc9f3c81e3d8494
Tim Harder <timothy.harder@analog.com>
2015-08-28 19:29:14 -0400

    post: split out elf load/run support

--- post/board/zynq/ad9361.c
+++ post/board/zynq/ad9361.c
@@ -9,9 +9,7 @@
  */
 
 #include <post.h>
-#include <common.h>
-#include <fs.h>
-#include <bootm.h>
+#include "run_elf.h"
 
 #if CONFIG_POST & CONFIG_SYS_POST_AD9361
 
@@ -20,30 +18,9 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-static char *load_params[] = {"fatload", "mmc", "0", LOAD_ADDR, LOAD_ELF};
-static char *boot_params[] = {"boote", LOAD_ADDR};
-
 int ad9361_post_test (int flags)
 {
-	int ret = 0;
-
-	ret = fs_set_blk_dev("mmc", "0", FS_TYPE_FAT);
-	if (ret != 0) {
-		printf("Failed accessing fat fs on the first MMC partition!\n");
-		goto finish;
-	}
-
-	ret = do_load(NULL, 0, 5, load_params, FS_TYPE_FAT);
-	if (ret != 0) {
-		printf("Failed loading %s from fat fs!\n", LOAD_ELF);
-		goto finish;
-	}
-
-	printf("Running %s\n", LOAD_ELF);
-	ret = do_bootelf(NULL, 0, 2, boot_params);
-
-finish:
-	return ret;
+	return run_elf(LOAD_ELF, LOAD_ADDR);
 }
 
 #endif /* CONFIG_POST & CONFIG_SYS_POST_AD9361 */
---
+++ post/board/zynq/run_elf.h
@@ -0,0 +1,7 @@
+/*
+ * Copyright (C) 2015 Analog Devices, Inc.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+int run_elf(char *path, char *addr);
