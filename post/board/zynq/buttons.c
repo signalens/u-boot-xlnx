f469b7d048f7d481523ffc16c1496c903d3864e1
Tim Harder <timothy.harder@analog.com>
2016-10-28 15:30:37 -0400

    zynq post: change push button test to work for what the pzsdr1 supports
    The pzsdr1 only routes 1 LED and 3 push button pins compared to the
    pzsdr2 which routes all 4 LEDS, 4 push buttons, and 4 switches.

--- post/board/zynq/buttons.c
+++ post/board/zynq/buttons.c
@@ -16,20 +16,12 @@
 
 #if CONFIG_POST & CONFIG_SYS_POST_BUTTONS
 
-#define LED0 0x3a
-#define LED1 0x3b
-#define LED2 0x3c
-#define LED3 0x3d
+// Only one LED and 3 push buttons have pins routed to them on the pzsdr1.
+#define LED0 0x39
 
-#define BUTTON0 0x36
-#define BUTTON1 0x37
-#define BUTTON2 0x38
-#define BUTTON3 0x39
-
-#define SWITCH0 0x3e
-#define SWITCH1 0x3f
-#define SWITCH2 0x40
-#define SWITCH3 0x41
+#define BUTTON1 0x36
+#define BUTTON2 0x37
+#define BUTTON3 0x38
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -39,23 +31,18 @@ int buttons_post_test (int flags)
 	unsigned long ts;
 	int gpios_off = 0;
 	int gpios_on = 0;
-	const int gpios[] = {
-		BUTTON0, BUTTON1, BUTTON2, BUTTON3,
-		SWITCH0, SWITCH1, SWITCH2, SWITCH3, -1};
+	const int gpios[] = {BUTTON1, BUTTON2, BUTTON3, -1};
 	int orig_gpio_values = gpio_get_values_as_int(gpios);
 	int gpio_values;
 
-	printf("\nToggle the buttons and switches on the board and watch for corresponding LED state changes.\n");
+	printf("\nToggle the buttons (1-3) on the board and watch for corresponding LED state changes.\n");
 	printf("The test will time out after 60 seconds if everything hasn't been toggled.\n");
 
 	ts = get_timer(0);
 
-	while ((gpios_off != 0xff) && (gpios_on != 0xff)) {
+	while ((gpios_off != 0x7) && (gpios_on != 0x7)) {
 		gpio_values = gpio_get_values_as_int(gpios);
-		gpio_direction_output(LED0, (gpio_values & 0x11));
-		gpio_direction_output(LED1, (gpio_values & 0x22));
-		gpio_direction_output(LED2, (gpio_values & 0x44));
-		gpio_direction_output(LED3, (gpio_values & 0x88));
+		gpio_direction_output(LED0, (gpio_values & 0x7));
 		if (gpio_values != orig_gpio_values) {
 			gpios_off |= ~(gpio_values);
 			gpios_on |= gpio_values;
